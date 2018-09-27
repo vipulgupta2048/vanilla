ae3ee8198b0652c3bb76351dea02e39541101a76
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2018-09-27 06:18:01 +0530

    THIS SI CHANGED Add HackMIT data
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

--- vanilla_bot/config.py
+++ vanilla_bot/config.py
@@ -1,10 +1,10 @@
 """
 Stores tokens and keys: imported by main.py
 """
-consumer_key = "Your Consumer Key here"
-consumer_secret = "Your Consumer Secret here"
-access_token = "Your Access Token here"
-access_token_secret = "Your Access Secret here"
+consumer_key = "IcqBkVowOsPrQW4y5ZTXQeUVm"
+consumer_secret = "gvbNKlfjkO8xdeaumvVJm8g5rfO3wv38l2TpQHvDPQNT8iHPdv"
+access_token = "749661321977819138-OTMNHPCkcyxsJ7S62vLKIf3LRkY4Uz8"
+access_token_secret = "UnabuFpDvBW3ieUBJOh0Ozadp64LNn1vm3IxzBbj84l5o"
 
 # add search terms of your choice
-searchterms = [""]
+searchterms = ["hackathon"]
--- vanilla_bot/vanilla.py
+++ vanilla_bot/vanilla.py
@@ -1,19 +1,34 @@
+# Importing libraies
 import time
 import tweepy
+import re
+import json
 
 # Twitter Access Tokens
 from config import *
 
-# Connecting to twitter service
+# Connecting/Authenticating our module with Twitter API
 AUTH = tweepy.OAuthHandler(consumer_key, consumer_secret)
 AUTH.set_access_token(access_token, access_token_secret)
 API = tweepy.API(AUTH)
 
-print("Finding Tweets with Vanilla \n")
-while True:
-    for searchterm in searchterms:
-        for tweet in tweepy.Cursor(API.search, q=searchterm):
-            tweet.retweet()
-            tweet.favorite()
-            print("retweeted")
-            #time.sleep(15)
+for searchterm in searchterms:
+    for tweet in tweepy.Cursor(API.search, q = searchterm).items(0):
+        print(tweet)
+        break
+
+
+#~ a = API.user_timeline(["vipulgupta2048",1])
+#print(a)
+
+
+
+
+
+# Magic Begins here
+#~ print("Finding Tweets with Vanilla \n")
+#~ while True:
+    #~ for searchterm in searchterms:
+
+        #~  #~ print(tweet.id)
+            #~ print("retweeted")
